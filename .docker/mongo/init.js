dac39694ca8841f080a2c0021adf321b1ba03a71
Cássio Paixão <cassiopaixaoti@gmail.com>
2021-06-27 13:18:22 -0300

    create resource routes

---
+++ .docker/mongo/init.js
@@ -0,0 +1,9 @@
+db.routes.insertMany([
+  {
+    _id: 1,
+    title: 'Primeiro',
+    startPosition: { lat: -15.82594, lng: -47.92923 },
+    endPosition: { lat: -15.82594, lng: -47.92923 },
+  }
+])
+
